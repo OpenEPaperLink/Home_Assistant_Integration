e7dbc3a49b3a1aed569912c5d20147785311d2a2
Jonas Niesner <jonas.niesner@gmail.com>
2023-03-31 08:46:49 +0200

    Initial commit

---
+++ custom_components/open_epaper_link/__init__.py
@@ -0,0 +1,63 @@
+from __future__ import annotations
+from .imagegen import *
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from . import hub
+from .const import DOMAIN
+import logging
+_LOGGER = logging.getLogger(__name__)
+
+PLATFORMS: list[str] = []
+
+def setup(hass, config):
+    #callback for the test service
+    async def teststervice(call) -> None:
+        mac = call.data.get("mac", "000000000000")
+        text = call.data.get("text", "")
+        ip = hass.states.get(DOMAIN + ".ip").state
+        imgbuff = genimg(text)
+        result = await hass.async_add_executor_job(uploadimg,imgbuff,mac,ip)
+    #callback for the image downlaod service
+    async def dlimg(call) -> None:
+        mac = call.data.get("mac", "000000000000")
+        url = call.data.get("url", "")
+        rotate = call.data.get("rotation", 0)
+        ip = hass.states.get(DOMAIN + ".ip").state
+        #we neet to know the esl type for resizing
+        hwtype = hass.states.get(DOMAIN + "." + mac.lower() + "hwtype").state
+        imgbuff = await hass.async_add_executor_job(downloadimg,url,hwtype,rotate)
+        result = await hass.async_add_executor_job(uploadimg,imgbuff,mac,ip)
+    #callback for the 5 line service
+    async def lines5service(call) -> None:
+        mac = call.data.get("mac", "000000000000")
+        line1 = call.data.get("line1", "")
+        line2 = call.data.get("line2", "")
+        line3 = call.data.get("line3", "")
+        line4 = call.data.get("line4", "")
+        line5 = call.data.get("line5", "")
+        border = call.data.get("border", "w")
+        format1 = call.data.get("format1", "mwwb")
+        format2 = call.data.get("format2", "mwwb")
+        format3 = call.data.get("format3", "mwwb")
+        format4 = call.data.get("format4", "mwwb")
+        format5 = call.data.get("format5", "mwwb")
+        ip = hass.states.get(DOMAIN + ".ip").state
+        imgbuff = gen5line(line1,line2,line3,line4,line5,border,format1,format2,format3,format4,format5)
+        result = await hass.async_add_executor_job(uploadimg,imgbuff,mac,ip)
+    #register the services
+    hass.services.register(DOMAIN, "set_tosensor", teststervice)
+    hass.services.register(DOMAIN, "dlimg", dlimg)
+    hass.services.register(DOMAIN, "lines5", lines5service)
+    #error haneling needs to be improved
+    return True
+        
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = hub.Hub(hass, entry.data["host"])
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
---
+++ custom_components/open_epaper_link/hub.py
@@ -0,0 +1,100 @@
+from __future__ import annotations
+import asyncio
+import random
+import websocket
+import socket
+import aiohttp
+import async_timeout
+import backoff
+import time
+import json
+import logging
+from threading import Thread
+from typing import Any
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers import device_registry as dr
+from .const import DOMAIN
+_LOGGER: Final = logging.getLogger(__name__)
+#Hub class for handeling communication
+class Hub:
+    #the init function starts the tread for all other communication
+    def __init__(self, hass: HomeAssistant, host: str) -> None:
+        self._host = host
+        self._hass = hass
+        self._name = host
+        self._id = host.lower()
+        self.esls = []
+        thread = Thread(target=self.establish_connection)
+        thread.start()
+        self.online = True
+    #parses websocket messages
+    def on_message(self,ws, message) -> None:
+        print(message)
+        data = json.loads(message)
+        if 'sys' in data:
+            sys = data.get('sys')
+            systime = sys.get('currtime')
+            #self._hass.bus.fire("eslmsg", {"sys": sys})
+            self._hass.states.set(DOMAIN + ".lastsl", systime)
+            self._hass.states.set(DOMAIN + ".ip", self._host)
+        elif 'tags' in data:
+            tag = data.get('tags')[0]
+            #self._hass.bus.fire("eslmsg", {"tag": tag})
+            tagmac = tag.get('mac')
+            lastseen = tag.get('lastseen')
+            nextupdate = tag.get('nextupdate')
+            nextcheckin = tag.get('nextcheckin')
+            LQI = tag.get('LQI')
+            RSSI = tag.get('RSSI')
+            temperature = tag.get('temperature')
+            batteryMv = tag.get('batteryMv')
+            pending = tag.get('pending')
+            hwType = tag.get('hwType')
+            contentMode = tag.get('contentMode')
+            #this needs to be improved
+            self._hass.states.set(DOMAIN + "." + tagmac + "lastseen", lastseen)
+            self._hass.states.set(DOMAIN + "." + tagmac + "nextupdate", nextupdate)
+            self._hass.states.set(DOMAIN + "." + tagmac + "nextcheckin", nextcheckin)
+            self._hass.states.set(DOMAIN + "." + tagmac + "lqi", LQI)
+            self._hass.states.set(DOMAIN + "." + tagmac + "rssi", RSSI)
+            self._hass.states.set(DOMAIN + "." + tagmac + "temperature", temperature)
+            self._hass.states.set(DOMAIN + "." + tagmac + "batterymv", batteryMv)
+            self._hass.states.set(DOMAIN + "." + tagmac + "pending", pending)
+            self._hass.states.set(DOMAIN + "." + tagmac + "hwtype", hwType)
+            self._hass.states.set(DOMAIN + "." + tagmac + "contentMode", contentMode)
+            #maintains a list of all tags, new entetys should be generated here
+            if tagmac not in self.esls:
+                self.esls.append(tagmac)
+        elif 'errMsg' in data:
+            ermsg = data.get('errMsg');
+            #self._hass.bus.fire("eslmsg", {"error": ermsg})
+        elif 'logMsg' in data:
+            logmsg = data.get('logMsg');
+            #self._hass.bus.fire("eslmsg", {"error": logmsg})
+        else:
+            _LOGGER.warning("Unknown msg")
+            _LOGGER.warning(data)
+    #log websocket errors
+    def on_error(self,ws, error) -> None:
+        _LOGGER.warning("Websocket error")
+        _LOGGER.warning(error)
+    #try to reconnect after 5 munutes
+    def on_close(self,ws, error, a) -> None:
+        _LOGGER.warning("Websocket connection lost")
+        print("Connection lost")
+        print("Waiting 300 seconds")
+        time.sleep(300)
+        self.establish_connection(self)
+    #we could do smething here
+    def on_open(self,ws) -> None:
+        time.sleep(1)
+    #starts the websocket
+    def establish_connection(self) -> None:
+        ws_url = "ws://" + self._host + "/ws"
+        ws = websocket.WebSocketApp(ws_url,on_message=self.on_message,on_error=self.on_error,on_close=self.on_close,on_open=self.on_open)
+        ws.run_forever()
+        _LOGGER.warning("This should not happen")
+    #we should do more here
+    async def test_connection(self) -> bool:
+        return True
