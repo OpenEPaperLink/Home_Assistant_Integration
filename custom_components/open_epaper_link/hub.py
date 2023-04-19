f45311b914c7b5d6906aef6e55f92de3e48e8c34
Jonas Niesner <jonas.niesner@gmail.com>
2023-04-19 15:28:13 +0200

    Update hub.py

--- custom_components/open_epaper_link/hub.py
+++ custom_components/open_epaper_link/hub.py
@@ -19,12 +19,22 @@ _LOGGER: Final = logging.getLogger(__name__)
 #Hub class for handeling communication
 class Hub:
     #the init function starts the tread for all other communication
-    def __init__(self, hass: HomeAssistant, host: str) -> None:
+    def __init__(self, hass: HomeAssistant, host: str,cfgenty: str) -> None:
         self._host = host
+        self._cfgenty = cfgenty
         self._hass = hass
         self._name = host
         self._id = host.lower()
         self.esls = []
+        self.data = dict()
+        self.data["ap"] = dict()
+        self.data["ap"]["ip"] =  self._host;
+        self.data["ap"]["systime"] = None;
+        self.data["ap"]["heap"] = None;
+        self.data["ap"]["recordcount"] = None;
+        self.data["ap"]["dbsize"] = None;
+        self.data["ap"]["littlefsfree"] = None;
+        self.eventloop = asyncio.get_event_loop()
         thread = Thread(target=self.establish_connection)
         thread.start()
         self.online = True
@@ -34,8 +44,18 @@ class Hub:
         if 'sys' in data:
             sys = data.get('sys')
             systime = sys.get('currtime')
-            self._hass.states.set(DOMAIN + ".lastsl", systime,{"icon": "mdi:clock-outline","friendly_name": "Last Syslog MSG","should_poll": False})
+            heap = sys.get('heap')
+            recordcount = sys.get('recordcount')
+            dbsize = sys.get('dbsize')
+            littlefsfree = sys.get('littlefsfree')
             self._hass.states.set(DOMAIN + ".ip", self._host,{"icon": "mdi:ip","friendly_name": "AP IP","should_poll": False})
+            self.data["ap"] = dict()
+            self.data["ap"]["ip"] =  self._host;
+            self.data["ap"]["systime"] = systime;
+            self.data["ap"]["heap"] = heap;
+            self.data["ap"]["recordcount"] = recordcount;
+            self.data["ap"]["dbsize"] = dbsize;
+            self.data["ap"]["littlefsfree"] = littlefsfree;
         elif 'tags' in data:
             tag = data.get('tags')[0]
             tagmac = tag.get('mac')
@@ -49,20 +69,35 @@ class Hub:
             pending = tag.get('pending')
             hwType = tag.get('hwType')
             contentMode = tag.get('contentMode')
-            #this needs to be improved
-            self._hass.states.set(DOMAIN + "." + tagmac + "lastseen", lastseen,{"icon": "mdi:clock-outline","friendly_name": "Last seen","should_poll": False})
-            self._hass.states.set(DOMAIN + "." + tagmac + "nextupdate", nextupdate,{"icon": "mdi:clock-outline","friendly_name": "Next Update","should_poll": False})
-            self._hass.states.set(DOMAIN + "." + tagmac + "nextcheckin", nextcheckin,{"icon": "mdi:clock-outline","friendly_name": "Next Checkin","should_poll": False})
-            self._hass.states.set(DOMAIN + "." + tagmac + "lqi", LQI,{"icon": "mdi:network-strength-1","friendly_name": "Link quality index","should_poll": False})
-            self._hass.states.set(DOMAIN + "." + tagmac + "rssi", RSSI,{"icon": "mdi:network-strength-1","friendly_name": "RSSI","device_class": "signal_strength","unit_of_measurement": "dB","should_poll": False})
-            self._hass.states.set(DOMAIN + "." + tagmac + "temperature", temperature,{"icon": "mdi:thermometer","friendly_name": "Temperature","device_class": "temperature","unit_of_measurement": "°C","should_poll": False})
-            self._hass.states.set(DOMAIN + "." + tagmac + "battery", batteryMv / 1000,{"icon": "mdi:battery","friendly_name": "Battery Voltage","device_class": "voltage","unit_of_measurement": "V","should_poll": False})
-            self._hass.states.set(DOMAIN + "." + tagmac + "pending", pending,{"icon": "mdi:fullscreen","friendly_name": "Pending content","should_poll": False})
+            alias = tag.get('alias')
+            wakeupReason = tag.get('wakeupReason')
+            capabilities = tag.get('capabilities')
+            hashv = tag.get('hash')
+            modecfgjson = tag.get('modecfgjson')
+            #required for automations
             self._hass.states.set(DOMAIN + "." + tagmac + "hwtype", hwType,{"icon": "mdi:fullscreen","friendly_name": "Hardware Type","should_poll": False})
-            self._hass.states.set(DOMAIN + "." + tagmac + "contentMode", contentMode,{"icon": "mdi:fullscreen","friendly_name": "Content mode","should_poll": False})
+            hwmap = {0: "1.54″ BWR",1: "2.9″ BWR",2: "4.2″ BWR"}
+            self.data[tagmac] = dict()
+            self.data[tagmac]["temperature"] = temperature
+            self.data[tagmac]["rssi"] = RSSI
+            self.data[tagmac]["battery"] = batteryMv
+            self.data[tagmac]["lqi"] = LQI
+            self.data[tagmac]["hwtype"] = hwType
+            self.data[tagmac]["hwstring"] = hwmap[hwType]
+            self.data[tagmac]["contentmode"] = contentMode
+            self.data[tagmac]["lastseen"] = lastseen
+            self.data[tagmac]["nextupdate"] = nextupdate
+            self.data[tagmac]["nextcheckin"] = nextcheckin
+            self.data[tagmac]["pending"] = pending
+            self.data[tagmac]["alias"] = alias
+            self.data[tagmac]["wakeupReason"] = wakeupReason
+            self.data[tagmac]["capabilities"] = capabilities
+            self.data[tagmac]["hashv"] = hashv
             #maintains a list of all tags, new entetys should be generated here
             if tagmac not in self.esls:
                 self.esls.append(tagmac)
+                loop = self.eventloop
+                asyncio.run_coroutine_threadsafe(self.reloadcfgett(),loop)
         elif 'errMsg' in data:
             ermsg = data.get('errMsg');
         elif 'logMsg' in data:
@@ -92,4 +127,9 @@ class Hub:
         _LOGGER.warning("This should not happen")
     #we should do more here
     async def test_connection(self) -> bool:
+        return True
+    #reload is reqired to add new entitys
+    async def reloadcfgett(self) -> bool:
+        await self._hass.config_entries.async_unload_platforms(self._cfgenty, ["sensor"])
+        await self._hass.config_entries.async_forward_entry_setups(self._cfgenty, ["sensor"])
         return True
