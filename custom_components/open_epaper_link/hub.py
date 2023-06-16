d09a5aa9fcd2e153cf7ae288f41553acf4fd41d0
gabriel <g.lackermeier@gmx.de>
2023-06-16 13:13:20 +0200

    Changed Indent Optimized Imports

--- custom_components/open_epaper_link/hub.py
+++ custom_components/open_epaper_link/hub.py
@@ -1,25 +1,23 @@
 from __future__ import annotations
+
 import asyncio
-import random
-import websocket
-import socket
-import aiohttp
-import async_timeout
-import backoff
-import time
 import json
 import logging
+import time
 from threading import Thread
-from typing import Any
+
+import websocket
 from homeassistant.core import HomeAssistant
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.helpers import device_registry as dr
+
 from .const import DOMAIN
+
 _LOGGER: Final = logging.getLogger(__name__)
-#Hub class for handeling communication
+
+
+# Hub class for handeling communication
 class Hub:
-    #the init function starts the thread for all other communication
-    def __init__(self, hass: HomeAssistant, host: str,cfgenty: str) -> None:
+    # the init function starts the thread for all other communication
+    def __init__(self, hass: HomeAssistant, host: str, cfgenty: str) -> None:
         self._host = host
         self._cfgenty = cfgenty
         self._hass = hass
@@ -28,7 +26,7 @@ class Hub:
         self.esls = []
         self.data = dict()
         self.data["ap"] = dict()
-        self.data["ap"]["ip"] =  self._host;
+        self.data["ap"]["ip"] = self._host;
         self.data["ap"]["systime"] = None;
         self.data["ap"]["heap"] = None;
         self.data["ap"]["recordcount"] = None;
@@ -38,8 +36,9 @@ class Hub:
         thread = Thread(target=self.establish_connection)
         thread.start()
         self.online = True
-    #parses websocket messages
-    def on_message(self,ws, message) -> None:
+
+    # parses websocket messages
+    def on_message(self, ws, message) -> None:
         data = json.loads(message)
         if 'sys' in data:
             sys = data.get('sys')
@@ -48,9 +47,10 @@ class Hub:
             recordcount = sys.get('recordcount')
             dbsize = sys.get('dbsize')
             littlefsfree = sys.get('littlefsfree')
-            self._hass.states.set(DOMAIN + ".ip", self._host,{"icon": "mdi:ip","friendly_name": "AP IP","should_poll": False})
+            self._hass.states.set(DOMAIN + ".ip", self._host,
+                                  {"icon": "mdi:ip", "friendly_name": "AP IP", "should_poll": False})
             self.data["ap"] = dict()
-            self.data["ap"]["ip"] =  self._host;
+            self.data["ap"]["ip"] = self._host;
             self.data["ap"]["systime"] = systime;
             self.data["ap"]["heap"] = heap;
             self.data["ap"]["recordcount"] = recordcount;
@@ -74,9 +74,10 @@ class Hub:
             capabilities = tag.get('capabilities')
             hashv = tag.get('hash')
             modecfgjson = tag.get('modecfgjson')
-            #required for automations
-            self._hass.states.set(DOMAIN + "." + tagmac + "hwtype", hwType,{"icon": "mdi:fullscreen","friendly_name": "Hardware Type","should_poll": False})
-            hwmap = {0: "1.54″ BWR",1: "2.9″ BWR",2: "4.2″ BWR"}
+            # required for automations
+            self._hass.states.set(DOMAIN + "." + tagmac + "hwtype", hwType,
+                                  {"icon": "mdi:fullscreen", "friendly_name": "Hardware Type", "should_poll": False})
+            hwmap = {0: "1.54″ BWR", 1: "2.9″ BWR", 2: "4.2″ BWR"}
             self.data[tagmac] = dict()
             self.data[tagmac]["temperature"] = temperature
             self.data[tagmac]["rssi"] = RSSI
@@ -93,11 +94,11 @@ class Hub:
             self.data[tagmac]["wakeupReason"] = wakeupReason
             self.data[tagmac]["capabilities"] = capabilities
             self.data[tagmac]["hashv"] = hashv
-            #maintains a list of all tags, new entities should be generated here
+            # maintains a list of all tags, new entities should be generated here
             if tagmac not in self.esls:
                 self.esls.append(tagmac)
                 loop = self.eventloop
-                asyncio.run_coroutine_threadsafe(self.reloadcfgett(),loop)
+                asyncio.run_coroutine_threadsafe(self.reloadcfgett(), loop)
         elif 'errMsg' in data:
             ermsg = data.get('errMsg');
         elif 'logMsg' in data:
@@ -105,30 +106,37 @@ class Hub:
         else:
             _LOGGER.warning("Unknown msg")
             _LOGGER.warning(data)
-    #log websocket errors
-    def on_error(self,ws, error) -> None:
+
+    # log websocket errors
+    def on_error(self, ws, error) -> None:
         _LOGGER.warning("Websocket error")
         _LOGGER.warning(error)
-    #try to reconnect after 5 munutes
-    def on_close(self,ws, error, a) -> None:
+
+    # try to reconnect after 5 munutes
+    def on_close(self, ws, error, a) -> None:
         _LOGGER.warning("Websocket connection lost")
         print("Connection lost")
         print("Waiting 300 seconds")
         time.sleep(300)
         self.establish_connection()
-    #we could do something here
-    def on_open(self,ws) -> None:
+
+    # we could do something here
+    def on_open(self, ws) -> None:
         time.sleep(1)
-    #starts the websocket
+
+    # starts the websocket
     def establish_connection(self) -> None:
         ws_url = "ws://" + self._host + "/ws"
-        ws = websocket.WebSocketApp(ws_url,on_message=self.on_message,on_error=self.on_error,on_close=self.on_close,on_open=self.on_open)
+        ws = websocket.WebSocketApp(ws_url, on_message=self.on_message, on_error=self.on_error, on_close=self.on_close,
+                                    on_open=self.on_open)
         ws.run_forever()
         _LOGGER.warning("This should not happen")
-    #we should do more here
+
+    # we should do more here
     async def test_connection(self) -> bool:
         return True
-    #reload is reqired to add new entities
+
+    # reload is reqired to add new entities
     async def reloadcfgett(self) -> bool:
         await self._hass.config_entries.async_unload_platforms(self._cfgenty, ["sensor"])
         await self._hass.config_entries.async_forward_entry_setups(self._cfgenty, ["sensor"])
--- custom_components/open_epaper_link/imagegen.py
+++ custom_components/open_epaper_link/imagegen.py
@@ -1,168 +1,186 @@
 from __future__ import annotations
-from PIL import Image, ImageDraw, ImageFont
-import requests
+
 import io
-from requests_toolbelt.multipart.encoder import MultipartEncoder
 import logging
-from pathlib import Path
 import os
+
+import requests
+from PIL import Image, ImageDraw, ImageFont
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+
 _LOGGER = logging.getLogger(__name__)
 
-#font imports
+# font imports
 rbmp = os.path.join(os.path.dirname(__file__), 'rbm.ttf')
 ppbp = os.path.join(os.path.dirname(__file__), 'ppb.ttf')
 rbm = ImageFont.truetype(rbmp, 11)
 ppb = ImageFont.truetype(ppbp, 23)
-#color definitions for picture generation
+# color definitions for picture generation
 red = (255, 0, 0)
 white = (255, 255, 255)
 black = (0, 0, 0)
 splitth = 147
 splitth2 = 280
-size0 = [152,152]
-size1 = [296,128]
-size2 = [400,300]
-#img downloader
-def downloadimg(url,hwtype,rotate):
-    #load the res of the esl
+size0 = [152, 152]
+size1 = [296, 128]
+size2 = [400, 300]
+
+
+# img downloader
+def downloadimg(url, hwtype, rotate):
+    # load the res of the esl
     res = getres(hwtype)
     response = requests.get(url)
     img = Image.open(io.BytesIO(response.content))
     if img.mode != 'RGB':
         img = img.convert('RGB')
     if rotate != 0:
-        img = img.rotate(rotate,expand = 1)
+        img = img.rotate(rotate, expand=1)
     width, height = img.size
-    #swap for big display
+    # swap for big display
     if hwtype == "2":
         b = res[0]
         res[0] = res[1]
         res[1] = b
-    #open esl expects the image rotated
+    # open esl expects the image rotated
     if width == res[1] and height == res[0]:
         print("all clear, pass")
     elif width == res[0] and height == res[1]:
         print("90 degree rotation needed")
-        img = img.rotate(90,expand = 1)
+        img = img.rotate(90, expand=1)
     else:
         print("rotate + resize required")
-        img = img.rotate(90,expand = 1)
-        img = img.resize((res[1],res[0]))    
+        img = img.rotate(90, expand=1)
+        img = img.resize((res[1], res[0]))
     buf = io.BytesIO()
     img.save(buf, format='JPEG')
     byte_im = buf.getvalue()
-    #img.save('img.png')
+    # img.save('img.png')
     return byte_im
-#hw type to size converter
+
+
+# hw type to size converter
 def getres(hwtype):
-   if hwtype == "0":
-       return size0
-   if hwtype == "1":
-       return size1
-   if hwtype == "2":
-       return size2
-#5 line text generator for 1.54 esls
-def gen5line(line1,line2,line3,line4,line5,border,format1,format2,format3,format4,format5):
-   w = 152
-   h = 152
-   img = Image.new('RGB', (w, h), color = white)
-   d = ImageDraw.Draw(img)
-   #we don't want interpolation
-   d.fontmode = "1"
-   #border
-   d.rectangle([(0, 0), (w - 1,h - 1)], fill = white, outline = chartocol(border))
-   #text backgrounds
-   d.rectangle([(1, 1), (150,30)], fill = chartocol(format1[1]), outline = chartocol(format1[2]))
-   d.rectangle([(1, 31), (150,60)], fill = chartocol(format2[1]), outline = chartocol(format2[2]))
-   d.rectangle([(1, 61), (150,90)], fill = chartocol(format3[1]), outline = chartocol(format3[2]))
-   d.rectangle([(1, 91), (150,120)], fill = chartocol(format4[1]), outline = chartocol(format4[2]))
-   d.rectangle([(1, 121), (150,150)], fill = chartocol(format5[1]), outline = chartocol(format5[2]))
-   #text lines
-   d =  textgen(d,line1,chartocol(format1[3]),format1[0],0)
-   d =  textgen(d,line2,chartocol(format2[3]),format2[0],30)
-   d =  textgen(d,line3,chartocol(format3[3]),format3[0],60)
-   d =  textgen(d,line4,chartocol(format4[3]),format4[0],90)
-   d =  textgen(d,line5,chartocol(format5[3]),format5[0],120)
-   buf = io.BytesIO()
-   img.save(buf, format='JPEG', quality=95)
-   byte_im = buf.getvalue()
-   #img.save('img.png')
-   return byte_im
-def gen4line(line1,line2,line3,line4,border,format1,format2,format3,format4):
-   w = 296
-   h = 128
-   img = Image.new('RGB', (w, h), color = white)
-   d = ImageDraw.Draw(img)
-   #we don't want interpolation
-   d.fontmode = "1"
-   #border
-   d.rectangle([(0, 0), (w - 2,h - 2)], fill = chartocol(border))
-   #text backgrounds
-   d.rectangle([(2, 2), (292,32)], fill = chartocol(format1[1]), outline = chartocol(format1[2]))
-   d.rectangle([(2, 33), (292,63)], fill = chartocol(format2[1]), outline = chartocol(format2[2]))
-   d.rectangle([(2, 64), (292,94)], fill = chartocol(format3[1]), outline = chartocol(format3[2]))
-   d.rectangle([(2, 95), (292,124)], fill = chartocol(format4[1]), outline = chartocol(format4[2]))
-   #text lines
-   d =  textgen2(d,line1,chartocol(format1[3]),format1[0],2)
-   d =  textgen2(d,line2,chartocol(format2[3]),format2[0],33)
-   d =  textgen2(d,line3,chartocol(format3[3]),format3[0],64)
-   d =  textgen2(d,line4,chartocol(format4[3]),format4[0],95)
-   buf = io.BytesIO()
-   img.save(buf, format='JPEG', quality=95)
-   byte_im = buf.getvalue()
-   #img.save('img.png')
-   return byte_im
-#handles Text alignment
-def textgen(d,text,col,just,yofs):
-   x = 76
-   if just == "l":
-       x = 3
-   if just == "r":
-       x = 147
-   if "\n" in text:
-       split1 = text.split("\n")[0]
-       split2 = text.split("\n")[1]
-       d.text((x,8 + yofs), split1, fill=col, anchor=just+"m", font=rbm)
-       d.text((x,22 + yofs),split2, fill=col, anchor=just+"m", font=rbm)
-   elif d.textlength(text, font=ppb) < splitth:
-       d.text((x,15 + yofs), text, fill=col, anchor=just+"m", font=ppb)
-   else:
-       d.text((x,15 + yofs), text, fill=col, anchor=just+"m", font=rbm)
-   return d
-#handles Text alignment
-def textgen2(d,text,col,just,yofs):
-   x = 148
-   if just == "l":
-       x = 3
-   if just == "r":
-       x = 290
-   if "\n" in text:
-       split1 = text.split("\n")[0]
-       split2 = text.split("\n")[1]
-       d.text((x,8 + yofs), split1, fill=col, anchor=just+"m", font=rbm)
-       d.text((x,22 + yofs),split2, fill=col, anchor=just+"m", font=rbm)
-   elif d.textlength(text, font=ppb) < splitth2:
-       d.text((x,15 + yofs), text, fill=col, anchor=just+"m", font=ppb)
-   else:
-       d.text((x,15 + yofs), text, fill=col, anchor=just+"m", font=rbm)
-   return d
-#converts a char to a color
+    if hwtype == "0":
+        return size0
+    if hwtype == "1":
+        return size1
+    if hwtype == "2":
+        return size2
+
+
+# 5 line text generator for 1.54 esls
+def gen5line(line1, line2, line3, line4, line5, border, format1, format2, format3, format4, format5):
+    w = 152
+    h = 152
+    img = Image.new('RGB', (w, h), color=white)
+    d = ImageDraw.Draw(img)
+    # we don't want interpolation
+    d.fontmode = "1"
+    # border
+    d.rectangle([(0, 0), (w - 1, h - 1)], fill=white, outline=chartocol(border))
+    # text backgrounds
+    d.rectangle([(1, 1), (150, 30)], fill=chartocol(format1[1]), outline=chartocol(format1[2]))
+    d.rectangle([(1, 31), (150, 60)], fill=chartocol(format2[1]), outline=chartocol(format2[2]))
+    d.rectangle([(1, 61), (150, 90)], fill=chartocol(format3[1]), outline=chartocol(format3[2]))
+    d.rectangle([(1, 91), (150, 120)], fill=chartocol(format4[1]), outline=chartocol(format4[2]))
+    d.rectangle([(1, 121), (150, 150)], fill=chartocol(format5[1]), outline=chartocol(format5[2]))
+    # text lines
+    d = textgen(d, line1, chartocol(format1[3]), format1[0], 0)
+    d = textgen(d, line2, chartocol(format2[3]), format2[0], 30)
+    d = textgen(d, line3, chartocol(format3[3]), format3[0], 60)
+    d = textgen(d, line4, chartocol(format4[3]), format4[0], 90)
+    d = textgen(d, line5, chartocol(format5[3]), format5[0], 120)
+    buf = io.BytesIO()
+    img.save(buf, format='JPEG', quality=95)
+    byte_im = buf.getvalue()
+    # img.save('img.png')
+    return byte_im
+
+
+def gen4line(line1, line2, line3, line4, border, format1, format2, format3, format4):
+    w = 296
+    h = 128
+    img = Image.new('RGB', (w, h), color=white)
+    d = ImageDraw.Draw(img)
+    # we don't want interpolation
+    d.fontmode = "1"
+    # border
+    d.rectangle([(0, 0), (w - 2, h - 2)], fill=chartocol(border))
+    # text backgrounds
+    d.rectangle([(2, 2), (292, 32)], fill=chartocol(format1[1]), outline=chartocol(format1[2]))
+    d.rectangle([(2, 33), (292, 63)], fill=chartocol(format2[1]), outline=chartocol(format2[2]))
+    d.rectangle([(2, 64), (292, 94)], fill=chartocol(format3[1]), outline=chartocol(format3[2]))
+    d.rectangle([(2, 95), (292, 124)], fill=chartocol(format4[1]), outline=chartocol(format4[2]))
+    # text lines
+    d = textgen2(d, line1, chartocol(format1[3]), format1[0], 2)
+    d = textgen2(d, line2, chartocol(format2[3]), format2[0], 33)
+    d = textgen2(d, line3, chartocol(format3[3]), format3[0], 64)
+    d = textgen2(d, line4, chartocol(format4[3]), format4[0], 95)
+    buf = io.BytesIO()
+    img.save(buf, format='JPEG', quality=95)
+    byte_im = buf.getvalue()
+    # img.save('img.png')
+    return byte_im
+
+
+# handles Text alignment
+def textgen(d, text, col, just, yofs):
+    x = 76
+    if just == "l":
+        x = 3
+    if just == "r":
+        x = 147
+    if "\n" in text:
+        split1 = text.split("\n")[0]
+        split2 = text.split("\n")[1]
+        d.text((x, 8 + yofs), split1, fill=col, anchor=just + "m", font=rbm)
+        d.text((x, 22 + yofs), split2, fill=col, anchor=just + "m", font=rbm)
+    elif d.textlength(text, font=ppb) < splitth:
+        d.text((x, 15 + yofs), text, fill=col, anchor=just + "m", font=ppb)
+    else:
+        d.text((x, 15 + yofs), text, fill=col, anchor=just + "m", font=rbm)
+    return d
+
+
+# handles Text alignment
+def textgen2(d, text, col, just, yofs):
+    x = 148
+    if just == "l":
+        x = 3
+    if just == "r":
+        x = 290
+    if "\n" in text:
+        split1 = text.split("\n")[0]
+        split2 = text.split("\n")[1]
+        d.text((x, 8 + yofs), split1, fill=col, anchor=just + "m", font=rbm)
+        d.text((x, 22 + yofs), split2, fill=col, anchor=just + "m", font=rbm)
+    elif d.textlength(text, font=ppb) < splitth2:
+        d.text((x, 15 + yofs), text, fill=col, anchor=just + "m", font=ppb)
+    else:
+        d.text((x, 15 + yofs), text, fill=col, anchor=just + "m", font=rbm)
+    return d
+
+
+# converts a char to a color
 def chartocol(c):
     if c == "r":
         return red
     if c == "w":
-        return white    
+        return white
     if c == "b":
         return black
-#upload an image to the tag
-def uploadimg(img,mac,ip):
-   url = "http://" + ip + "/imgupload"
-   mp_encoder = MultipartEncoder(
-       fields={
-           'mac': mac,
-           'image': ('image.jpg', img , 'image/jpeg'),
-       }
-   )
-   response = requests.post(url, headers={'Content-Type': mp_encoder.content_type}, data=mp_encoder)
-   if response.status_code != 200:
-       _LOGGER.warning(response.status_code)
+
+
+# upload an image to the tag
+def uploadimg(img, mac, ip):
+    url = "http://" + ip + "/imgupload"
+    mp_encoder = MultipartEncoder(
+        fields={
+            'mac': mac,
+            'image': ('image.jpg', img, 'image/jpeg'),
+        }
+    )
+    response = requests.post(url, headers={'Content-Type': mp_encoder.content_type}, data=mp_encoder)
+    if response.status_code != 200:
+        _LOGGER.warning(response.status_code)
