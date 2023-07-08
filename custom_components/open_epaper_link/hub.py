8a12bf5c561e7e6a31e15de53c12dec9de4ed9d5
Jonas Niesner <jonas.niesner@gmail.com>
2023-07-08 22:26:38 +0200

    Update hub.py

--- custom_components/open_epaper_link/hub.py
+++ custom_components/open_epaper_link/hub.py
@@ -1,23 +1,25 @@
 from __future__ import annotations
-
 import asyncio
+import random
+import websocket
+import socket
+import aiohttp
+import async_timeout
+import backoff
+import time
 import json
 import logging
-import time
 from threading import Thread
-
-import websocket
+from typing import Any
 from homeassistant.core import HomeAssistant
-
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers import device_registry as dr
 from .const import DOMAIN
-
 _LOGGER: Final = logging.getLogger(__name__)
-
-
-# Hub class for handeling communication
+#Hub class for handeling communication
 class Hub:
-    # the init function starts the thread for all other communication
-    def __init__(self, hass: HomeAssistant, host: str, cfgenty: str) -> None:
+    #the init function starts the thread for all other communication
+    def __init__(self, hass: HomeAssistant, host: str,cfgenty: str) -> None:
         self._host = host
         self._cfgenty = cfgenty
         self._hass = hass
@@ -26,7 +28,7 @@ class Hub:
         self.esls = []
         self.data = dict()
         self.data["ap"] = dict()
-        self.data["ap"]["ip"] = self._host;
+        self.data["ap"]["ip"] =  self._host;
         self.data["ap"]["systime"] = None;
         self.data["ap"]["heap"] = None;
         self.data["ap"]["recordcount"] = None;
@@ -36,9 +38,8 @@ class Hub:
         thread = Thread(target=self.establish_connection)
         thread.start()
         self.online = True
-
-    # parses websocket messages
-    def on_message(self, ws, message) -> None:
+    #parses websocket messages
+    def on_message(self,ws, message) -> None:
         data = json.loads(message)
         if 'sys' in data:
             sys = data.get('sys')
@@ -47,15 +48,26 @@ class Hub:
             recordcount = sys.get('recordcount')
             dbsize = sys.get('dbsize')
             littlefsfree = sys.get('littlefsfree')
-            self._hass.states.set(DOMAIN + ".ip", self._host,
-                                  {"icon": "mdi:ip", "friendly_name": "AP IP", "should_poll": False})
+            apstate = sys.get('apstate')
+            runstate = sys.get('runstate')
+            temp = sys.get('temp')
+            rssi = sys.get('rssi')
+            wifistatus = sys.get('wifistatus')
+            wifissid = sys.get('wifissid')
+            self._hass.states.set(DOMAIN + ".ip", self._host,{"icon": "mdi:ip","friendly_name": "AP IP","should_poll": False})
             self.data["ap"] = dict()
-            self.data["ap"]["ip"] = self._host;
+            self.data["ap"]["ip"] =  self._host;
             self.data["ap"]["systime"] = systime;
             self.data["ap"]["heap"] = heap;
             self.data["ap"]["recordcount"] = recordcount;
             self.data["ap"]["dbsize"] = dbsize;
             self.data["ap"]["littlefsfree"] = littlefsfree;
+            self.data["ap"]["rssi"] = rssi;
+            self.data["ap"]["apstate"] = apstate;
+            self.data["ap"]["runstate"] = runstate;
+            self.data["ap"]["temp"] = temp;
+            self.data["ap"]["wifistatus"] = wifistatus;
+            self.data["ap"]["wifissid"] = wifissid;
         elif 'tags' in data:
             tag = data.get('tags')[0]
             tagmac = tag.get('mac')
@@ -74,10 +86,14 @@ class Hub:
             capabilities = tag.get('capabilities')
             hashv = tag.get('hash')
             modecfgjson = tag.get('modecfgjson')
-            # required for automations
-            self._hass.states.set(DOMAIN + "." + tagmac + "hwtype", hwType,
-                                  {"icon": "mdi:fullscreen", "friendly_name": "Hardware Type", "should_poll": False})
-            hwmap = {0: "1.54″ BWR", 1: "2.9″ BWR", 2: "4.2″ BWR"}
+            isexternal = tag.get('isexternal')
+            rotate = tag.get('rotate')
+            lut = tag.get('lut')
+            ch = tag.get('ch')
+            ver = tag.get('ver')
+            #required for automations
+            self._hass.states.set(DOMAIN + "." + tagmac + "hwtype", hwType,{"icon": "mdi:fullscreen","friendly_name": "Hardware Type","should_poll": False})
+            hwmap = {0: "1.54″ BWR",1: "2.9″ BWR",2: "4.2″ BWR",17: "2.9″ BWR NFC",17: "2.9″ BWR NFC",240: "Segmented UK"}
             self.data[tagmac] = dict()
             self.data[tagmac]["temperature"] = temperature
             self.data[tagmac]["rssi"] = RSSI
@@ -90,53 +106,54 @@ class Hub:
             self.data[tagmac]["nextupdate"] = nextupdate
             self.data[tagmac]["nextcheckin"] = nextcheckin
             self.data[tagmac]["pending"] = pending
-            self.data[tagmac]["alias"] = alias
             self.data[tagmac]["wakeupReason"] = wakeupReason
             self.data[tagmac]["capabilities"] = capabilities
+            self.data[tagmac]["external"] = isexternal
+            self.data[tagmac]["alias"] = alias
             self.data[tagmac]["hashv"] = hashv
-            # maintains a list of all tags, new entities should be generated here
+            self.data[tagmac]["modecfgjson"] = modecfgjson
+            self.data[tagmac]["rotate"] = rotate
+            self.data[tagmac]["lut"] = lut
+            self.data[tagmac]["ch"] = ch
+            self.data[tagmac]["ver"] = ver
+            #maintains a list of all tags, new entities should be generated here
             if tagmac not in self.esls:
                 self.esls.append(tagmac)
                 loop = self.eventloop
-                asyncio.run_coroutine_threadsafe(self.reloadcfgett(), loop)
+                asyncio.run_coroutine_threadsafe(self.reloadcfgett(),loop)
         elif 'errMsg' in data:
             ermsg = data.get('errMsg');
         elif 'logMsg' in data:
             logmsg = data.get('logMsg');
+        elif 'apitem' in data:
+            logmsg = data.get('apitem');
         else:
             _LOGGER.warning("Unknown msg")
             _LOGGER.warning(data)
-
-    # log websocket errors
-    def on_error(self, ws, error) -> None:
-        _LOGGER.warning("Websocket error")
+    #log websocket errors
+    def on_error(self,ws, error) -> None:
+        _LOGGER.warning("Websocket error, most likely on_message crashed")
         _LOGGER.warning(error)
-
-    # try to reconnect after 5 munutes
-    def on_close(self, ws, error, a) -> None:
+    #try to reconnect after 5 munutes
+    def on_close(self,ws, error, a) -> None:
         _LOGGER.warning("Websocket connection lost")
         print("Connection lost")
         print("Waiting 300 seconds")
         time.sleep(300)
         self.establish_connection()
-
-    # we could do something here
-    def on_open(self, ws) -> None:
+    #we could do something here
+    def on_open(self,ws) -> None:
         time.sleep(1)
-
-    # starts the websocket
+    #starts the websocket
     def establish_connection(self) -> None:
         ws_url = "ws://" + self._host + "/ws"
-        ws = websocket.WebSocketApp(ws_url, on_message=self.on_message, on_error=self.on_error, on_close=self.on_close,
-                                    on_open=self.on_open)
+        ws = websocket.WebSocketApp(ws_url,on_message=self.on_message,on_error=self.on_error,on_close=self.on_close,on_open=self.on_open)
         ws.run_forever()
         _LOGGER.warning("This should not happen")
-
-    # we should do more here
+    #we should do more here
     async def test_connection(self) -> bool:
         return True
-
-    # reload is reqired to add new entities
+    #reload is reqired to add new entities
     async def reloadcfgett(self) -> bool:
         await self._hass.config_entries.async_unload_platforms(self._cfgenty, ["sensor"])
         await self._hass.config_entries.async_forward_entry_setups(self._cfgenty, ["sensor"])
